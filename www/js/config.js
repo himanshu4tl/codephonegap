fe2006c520eda28ce7940a39e8d37305fab081d6
himanshu <himanshu53939@gmail.com>
2016-02-01 16:58:44 +0530

    test

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={		
+	'ApiUrl':"http://myhalaleats.com/mobileapp/api",
+	'DialogDefaultTitle':"YOUR_OWN_DIALOG_TITLE",
+	'pushNotificationSenderid':"YOUR_ANDROID_PUSH_PROJECT_ID",
+	'facebookAppId':"796560520473152"
+};
