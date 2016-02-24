31a497b0e6cc22cd470e75b4718c530d03721bf4
himanshu4tl <himanshu53939@gmail.com>
2016-02-25 04:29:23 +0530

    test

--- www/js/index.js
+++ www/js/index.js
@@ -4,8 +4,8 @@ $(".button-collapse").sideNav();
 String.prototype.replaceAll = function(search, replacement) {return this.replace(new RegExp(search, 'g'), replacement);};
 
 var app={
-    defaultPage:'page1',
-    baseUrl:'http://localhost/test/angular/web',
+    defaultPage:'site/login?t=p',
+    baseUrl:'http://localhost/test/angular/web/',
     mainContainer:$('#contentView'),
     translateHtml:function(html,object){
         $.each(object,function(index,value){html=html.replaceAll(index,value);});return html;
@@ -14,10 +14,13 @@ var app={
         return this.translateHtml($('#'+templateID).html(),data);
     },
     loadDefaultPage:function(){
-      this.loadPage(this.defaultPage);
+      this.loadAjaxPage(this.defaultPage);
     },
     appInit:function(){
+        $('a').on('click',function(e){e.preventDefault();app.loadAjaxPage(this.href);});
         this.loadDefaultPage();
+        $('#slide-out a').on('click',function(){$('.button-collapse').sideNav('hide');});
+
     },
     renderHtml:function(html){
         this.mainContainer.html(html);
@@ -27,11 +30,24 @@ var app={
         console.log(templateId);
         this.renderHtml(this.creteHtml(templateId,{}));
     },
+    alert:function(msg){alert(msg);},
     loadAjaxPage:function(url){
-        $.get(this.baseUrl+'/'+url,function(response){app.renderHtml(response);})
+        console.log(url);
+        url=url.split('/')
+        $.get(this.baseUrl+url[url.length-2]+'/'+url[url.length-1],function(response){app.renderHtml(response);})
+    },
+    AjaxForm:function(obj){
+        var $this=$(obj);
+        $.ajax({url:$this.prop('action'),data:$this.serialize(),type:'post',datatype:'json',
+            success:function(response){if(response.message){app.alert(response.message);}
+                if(response.status){app.loadAjaxPage(response.url);}
+            },
+            error:function(e){console.log(e);}
+        })
     },
     pageInit:function(){
-        $('a').on('click',function(e){e.preventDefault();app.loadPage(this.rel);});
+        this.mainContainer.find('a').on('click',function(e){e.preventDefault();app.loadAjaxPage(this.href);});
+        this.mainContainer.find('form').on('submit',function(e){e.preventDefault();app.AjaxForm(this);});
     },
 
 };
