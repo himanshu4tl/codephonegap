79c230c07ce9258ad0f7e7dfa7b0c1ec1aef23fd
himanshu <himanshu53939@gmail.com>
2016-03-22 17:08:08 -0700

    gmap

--- www/js/index.js
+++ www/js/index.js
@@ -4,10 +4,10 @@ $(".button-collapse").sideNav();
 String.prototype.replaceAll = function(search, replacement) {return this.replace(new RegExp(search, 'g'), replacement);};
 
 var app={
-    defaultPage:'site/profile',
+    defaultPage:'profileTemplate',
     currentUrl:'',
-    baseUrl:'http://sateweb.com/gava/web/index.php/api/',
-    //baseUrl:'http://localhost/gava/web/api/',
+    //baseUrl:'http://sateweb.com/gava/web/index.php/api/',
+    baseUrl:'http://localhost/gava/web/api/',
     mainContainer:$('#contentView'),
     id:'',
     loader:$('#mLoader'),
@@ -17,13 +17,17 @@ var app={
     creteHtml:function(templateID,data){
         return this.translateHtml($('#'+templateID).html(),data);
     },
+    creteHtmlData:function(templateID,data){
+        var html="";
+        var htnlData=$('#'+templateID).html();
+        $.each(data,function(index,value){html+=app.translateHtml(htnlData,value);});return html;
+    },
     loadDefaultPage:function(){
-        if(this.id){this.loadAjaxPage(this.defaultPage);}else{app.loadPage('loginTemplate');app.currentUrl='site/index';app.afterRout();}
+        if(this.id){app.loadProfile($('#profileLink'));}else{app.loadPage('loginTemplate',$('#loginLink'));}
     },
     appInit:function(){
         $('#slide-out a').on('click',function(e){
             e.preventDefault();
-            app.loadAjaxPage(this.href);
             $('.button-collapse').sideNav('hide');
         });
         if(localStorage['id']){this.id=localStorage['id']}
@@ -34,33 +38,20 @@ var app={
         this.mainContainer.html(html);
         this.pageInit();
     },
-    loadPage:function(templateId){
-        console.log(templateId);
-        this.renderHtml(this.creteHtml(templateId,{}));
-        app.stopLoader();
-        this.afterLoadPage(templateId);
-    },
     alert:function(msg){Materialize.toast(msg,5000);},
-    loadAjaxPage:function(url){
-        url=url.split('/');
-        console.log(url[url.length-1]);
-        if(url[url.length-1]=='#'){return false;}
-        this.currentUrl=url[url.length-2]+'/'+url[url.length-1];
-        console.log(this.currentUrl);
+    callAjax:function(url,params,func){
         app.startLoader();
         $.ajax({
-            url:this.baseUrl+this.currentUrl+'?token='+app.id,
+            url:this.baseUrl+url+'?token='+app.id+params,
             type:'get',
-            success:function(response){
-                app.stopLoader();
-                app.renderHtml(response);
-                app.afterRout();
-            },
+            success:func,
             error:function(e){console.log(e);app.stopLoader();}
         });
     },
-    AjaxForm:function(obj){
+    postAjax:function(obj,func,params){
         var $this=$(obj);
+        $this.find('[type="submit"]').attr('disabled','true');
+        setTimeout(function(){$this.find('[type="submit"]').removeAttr('disabled');},5000);
         app.startLoader();
         var formData = new FormData($this[0]);
         formData.append('token',app.id);
@@ -69,7 +60,7 @@ var app={
         this.currentUrl=url[url.length-2]+'/'+url[url.length-1];
         console.log(this.currentUrl);
         $.ajax({
-            url:this.baseUrl+this.currentUrl+'?token='+app.id,
+            url:this.baseUrl+this.currentUrl+'?token='+app.id+params,
             data:formData,
             type:'post',
             datatype:'json',
@@ -77,24 +68,31 @@ var app={
             cache: false,
             contentType: false,
             processData: false,
-            success:function(response){
-                app.stopLoader();
-                if(response.message){app.alert(response.message);}
-                if(response.token){localStorage['id']=app.id=response.token;app.setUserLogin();}
-                if(response.email){app.email=response.email;}
-                if(response.status){app.loadAjaxPage(response.url);}
-            },
+            success:func,
             error:function(e){console.log(e);app.stopLoader();}
         })
     },
     pageInit:function(){
-        this.mainContainer.find('a').on('click',function(e){e.preventDefault();app.loadAjaxPage(this.href);});
-        this.mainContainer.find('form').on('submit',function(e){e.preventDefault();app.AjaxForm(this);});
+        this.mainContainer.find('a').on('click',function(e){e.preventDefault();});
+        this.mainContainer.find('form').on('submit',function(e){e.preventDefault();});
     },
     setUserLogout:function(){
-        localStorage['id']=app.id='';
         $('.auth').hide();
         $('.site-login').show();
+
+        var func=function(response){
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status) {
+                var obj=$('#loginLink');
+                app.loadPage('loginTemplate',obj);
+                $(obj).parent().addClass('active').siblings().removeClass('active');
+                localStorage['id']=app.id='';
+            }
+
+        };
+        app.callAjax('site/logout','',func);
+
     },
     setUserLogin:function(){
         $(".auth").show();
@@ -111,8 +109,14 @@ var app={
     stopLoader:function(){
         app.loader.hide();
     },
-    setProfileData:function(data){
-        $('#userLogo').html(app.creteHtml('userProfileTemplate',data));
+    loadPage:function(templateId,obj,data){
+        $(obj).parent().addClass('active').siblings().removeClass('active');
+        if(!data){data={}}
+        console.log(templateId);
+        this.renderHtml(this.creteHtml(templateId,data));
+        app.stopLoader();
+        this.pageInit();
+        this.afterLoadPage(templateId);
     },
     afterLoadPage:function(templateId){
         if(templateId=="loginTemplate"){
@@ -122,6 +126,92 @@ var app={
             $('#distance').val(localStorage['distance']);
         }
     },
+    setProfileData:function(data){
+        $('#userLogo').html(app.creteHtml('userProfileTemplate',data));
+    },
+
+    loadHome:function(obj){
+        app.loadPage('homeTemplate',obj);
+        $('#googleMap').css({height:window.screen.height,width:$('body').width()});
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            //x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    },
+    loadVault:function(obj){
+        var func=function(response){
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status){
+                app.loadPage('vaultTemplate',$('vaultLink'));
+                $('#vaultData').html(app.creteHtmlData('vaultDataTemplate',response.data));
+                $(obj).parent().addClass('active').siblings().removeClass('active');
+            }
+
+        };
+        app.callAjax('site/vault','',func);
+    },
+    loadProfile:function(obj){
+        var func=function(response){
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status) {
+                app.loadPage('profileTemplate', $('profileLink'), response.data);
+                $(obj).parent().addClass('active').siblings().removeClass('active');
+                app.setProfileData(response.data);
+                $('#rateit6').rateit({ max: 5, step: 0.5, backingfld: '#backing6' });
+            }
+
+        };
+        app.callAjax('site/profile','',func);
+    },
+    loginForm:function(obj){
+        var func=function(response){
+
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status){
+                if(response.token){localStorage['id']=app.id=response.token;app.setUserLogin();}
+                app.loadProfile($('#profileLink'));
+            }
+        };
+        this.postAjax(obj,func,'');
+    },
+    signupForm:function(obj){
+        var func=function(response){
+
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status){
+                app.loadPage('loginTemplate',$('#loginLink'));
+            }
+        };
+        this.postAjax(obj,func,'');
+    },
+    profileForm:function(obj){
+        var func=function(response){
+            app.stopLoader();
+            if(response.message){app.alert(response.message);}
+            if(response.status){app.loadProfile($('#profileLink'));}
+        };
+        this.postAjax(obj,func,'');
+    },
 
 };
 app.appInit();
+function showPosition(position) {
+    console.log(position);
+    var myCenter=new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: {lat: position.coords.latitude, lng: position.coords.longitude},
+        zoom: 8
+    });
+    var marker=new google.maps.Marker({
+        position:myCenter,
+    });
+
+    marker.setMap(map);
+
+}
